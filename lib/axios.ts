688e5fe29358c3d943012ab7e404214d5eaaa79f
anhphuc0508 <PhucNPA.B23CE078@stu.ptit.edu.vn>
2025-11-26 02:18:41 +0700

    cac

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-c9mb.onrender.com/api/v1',
+  baseURL: 'https://backend-c9mb.onrender.com/',
   withCredentials: false,
 });
 
