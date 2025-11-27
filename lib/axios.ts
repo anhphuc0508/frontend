270733420554bb296b14581af61ca1a4303577b4
anhphuc0508 <PhucNPA.B23CE078@stu.ptit.edu.vn>
2025-11-27 23:28:06 +0700

    final

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://backend-production-5dcb.up.railway.app/api/v1',
   withCredentials: false,
 });
 
